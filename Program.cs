e1f914e30443d5b2782fa8af79bf615d9dcc29a2
dubious123 <dubious123@hanyang.ac.kr>
2022-08-19 15:32:30 +0900

    Init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mockup_BrawlStars_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
