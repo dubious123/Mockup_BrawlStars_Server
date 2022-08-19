28692dbf73a95d7f03ffd2f056e185ed616b17ee
dubious123 <dubious123@hanyang.ac.kr>
2022-08-19 15:57:40 +0900

    feat : [WIP] add server endpoint

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ServerCore;
+using System;
+using System.Net;
 
 namespace Server
 {
@@ -6,7 +8,15 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+            var listener = new Listener();
+            //_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
+            Console.WriteLine("Listening...");
+
+
         }
     }
 }
